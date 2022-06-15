e1f3fa4957e77352b30efa21e0c012e446b17dc9
eliashhtores <eliashhtorres@gmail.com>
2022-06-15 10:47:25 -0500

    Added ChoiceField support to ConceptSerializer

--- concept/serializers.py
+++ concept/serializers.py
@@ -1,12 +1,31 @@
+from asyncore import read
 from rest_framework import serializers
 from .models import Concept
+
+
+class ChoiceField(serializers.ChoiceField):
+
+    def to_representation(self, obj):
+        if obj == '' and self.allow_blank:
+            return obj
+        return self._choices[obj]
+
+    def to_internal_value(self, data):
+        if data == '' and self.allow_blank:
+            return ''
+
+        for key, val in self._choices.items():
+            if val == data:
+                return key
+        self.fail('invalid_choice', input=data)
 
 
 class ConceptSerializer(serializers.ModelSerializer):
     """Serializer for concept objects"""
 
-    type = serializers.CharField(source='get_type_display')
+    type = ChoiceField(choices=Concept.CONCEPT_CHOICES)
 
     class Meta:
         model = Concept
         fields = '__all__'
+        read_only_fields = ('id',)
