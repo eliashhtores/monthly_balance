a1404bea640a10eddefd7629b2dfca65a5e2255d
eliashhtores <eliashhtorres@gmail.com>
2022-08-12 17:31:56 -0500

    Concepts must be unique

--- concept/models.py
+++ concept/models.py
@@ -13,5 +13,8 @@ class Concept(TimeStampedModel):
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=2, choices=CONCEPT_CHOICES)
 
+    class Meta:
+        unique_together = ('name', 'type')
+
     def __str__(self):
         return self.name
